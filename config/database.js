5089d2820e9943c6b0d605db22a4148376e9adc1
DiegoCP27 <dicornyt@gmail.com>
2023-06-15 21:47:15 -0500

    refactor: configurar la base de datos

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
+      host: env('PGHOST', 'containers-us-west-151.railway.app'),
+      port: env.int('PGPORT', 6136),
+      database: env('PGDATABASE', 'railway'),
+      user: env('PGUSER', 'postgres'),
+      password: env('PGPASSWORD', 'tDiurNzrgDn8lc7VfH6e'),
       ssl: env.bool(true),
     },
     pool: { min: 0 }
